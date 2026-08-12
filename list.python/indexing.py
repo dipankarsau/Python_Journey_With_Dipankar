f12da59a58f898492e2cc71f6d2cc7d16302a9c1
Dipankar Sau <akashsau2005@gmail.com>
2026-08-13 01:20:52 +0530

    add new file

---
+++ list.python/indexing.py
@@ -0,0 +1,16 @@
+# positive indexing : left to right 
+# Negative indexing : Right to left
+
+lst=["anirudh","samjay",56,91.8120,"surat"]
+# print(lst[0])
+# print(lst[1])
+# print(lst[-1])
+
+
+print(f" last element ={lst[4]}")
+
+
+n=len(lst)
+print(n)
+print(lst[n-1])
+
