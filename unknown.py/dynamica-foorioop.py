a0b6bda45838448aa317856393d7f2719089e2e0
Dipankar Sau <akashsau2005@gmail.com>
2026-08-12 23:50:11 +0530

    add new file

--- unknown.py/dynamica-foorioop.py
+++
@@ -1,4 +0,0 @@
-start=int(input('enter a nuumber'))
-end=int(input('enter a nuumber'))
-for  i in range(start,end+1):
-    print(i)
--- unknown.py/while_loop.py
+++
@@ -1,9 +0,0 @@
-# while 99>3:
-#     print("hello")
-#     # code runj ulimited time
-i=1
-while i<=2:
-    
-    print("hello")
-    i=i+1
-    print("done")
