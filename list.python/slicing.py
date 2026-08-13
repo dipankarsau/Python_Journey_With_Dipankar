92f9147cfd18839ce674538a67494a220ec7e065
Dipankar Sau <akashsau2005@gmail.com>
2026-08-14 00:53:23 +0530

    add new folder

--- list.python/slicing.py
+++ list.python/slicing.py
@@ -1,3 +1,21 @@
 nums=[10,20,30,40,50,60,70,80]
-print(nums[0:4])
-print(nums[5:9])
+# print(nums[0:4])
+# print(nums[5:9])
+# print(nums[1:88])
+# print(nums[5:6])
+# print(nums[5:5])
+# print(nums[4:])  
+# print(nums[:3])
+# print(nums[0:9:2])
+# print(nums[0::2])
+# print(nums[::2])
+# print(nums[:])
+
+# negative slicing
+lst1=nums[9:3:-1]
+lst1=nums[5:1:-1]
+lst1=nums[5::-1]
+lst1=nums[::-1]
+print(lst1)
+
+
