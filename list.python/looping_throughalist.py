ee6771840c7eed83b713654135538ead54dfc9db
Dipankar Sau <akashsau2005@gmail.com>
2026-08-14 22:19:15 +0530

    add new folder

---
+++ list.python/looping_throughalist.py
@@ -0,0 +1,15 @@
+# using a for loop
+# fruit=["apple","mango","banna"]
+# for i in fruit:
+#     print(i)
+  
+
+# using a while lopp
+fruit=["apple","mango","banna"]
+i=0
+while i<len(fruit):
+    print(fruit[i])
+    i=i+1
+
+
+# looping using enumerate()
