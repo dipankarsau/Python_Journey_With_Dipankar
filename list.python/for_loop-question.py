02a1c65cc56c82213723bb10374ef29e27067e22
Dipankar Sau <akashsau2005@gmail.com>
2026-08-14 22:47:37 +0530

    add new folder

---
+++ list.python/for_loop-question.py
@@ -0,0 +1,10 @@
+# num=[5,7,4,64,32,17,53,85,3,1,999]
+# for i in num:
+#     print(i)
+
+# reverse order
+num=[5,7,4,64,32,17,53,85,3,1,999]
+for i in range(len(num)-1,-1,-1):
+    print(num[i])
+
+
