908c56122a52f2dd68f0e25a978892de4f982634
Dipankar Sau <akashsau2005@gmail.com>
2026-08-14 23:15:20 +0530

    add new folder

--- list.python/for_loop-question.py
+++ list.python/for_loop-question.py
@@ -3,8 +3,30 @@
 #     print(i)
 
 # reverse order
-num=[5,7,4,64,32,17,53,85,3,1,999]
-for i in range(len(num)-1,-1,-1):
-    print(num[i])
+# num=[5,7,4,64,32,17,53,85,3,1,999]
+
+# for i in range(len(num)-1,-1,-1):
+#     print(num[i])
+#
+# another way of reverse
+# num=[5,7,4,64,32,17,53,85,3,1,999]
+# for i in num[::-1]:
+#     print(i)
+
+
+
+
+
+# print total number
+
+
+# num=[5,7,4,64,32,17,53,85,3,1,999]
+# total=0
+# for i in num:
+#     total+=i
+# print(total)
+    
+
+
 
 
--- list.python/looping_throughalist.py
+++ list.python/looping_throughalist.py
@@ -13,3 +13,15 @@ while i<len(fruit):
 
 
 # looping using enumerate()
+#use kora hoy jokhon value ar index duto chai
+
+# num=[5,7,4,64,32,17,53,85,3,1,999]
+# for index,value in enumerate(num):
+#     print(f' index value is {index}value is {value}')
+
+    # even number ar index print
+num = [5, 7, 4, 64, 32, 17, 53, 85, 3, 1, 999]
+
+for i, v in enumerate(num):
+    if v % 2 == 0:
+        print(i)
