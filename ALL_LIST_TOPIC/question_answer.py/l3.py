f313a404343e3515506caed8364c3a59f75814f2
Dipankar Sau <akashsau2005@gmail.com>
2026-08-15 13:10:16 +0530

    add new folder

--- ALL_LIST_TOPIC/question_answer.py/l3.py
+++ ALL_LIST_TOPIC/question_answer.py/l3.py
@@ -5,3 +5,19 @@
 # Hint: Use the + operator or a loop to
 # combine. Example: list1 = [1, 2], list2 = [3,
 # 4] -> merged = [1, 2, 3, 4]
+
+
+lst1=[1,2]
+lst2=[3,4]
+lst3=[]
+for i in lst1:
+    if i not in lst2:
+        lst3.append(i)
+for i in lst2:
+    if i not in lst1:
+        lst3.append(i)
+print(lst3)
+
+
+
+
