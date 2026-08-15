14a29978023695bd761d13009843c0b09bcad3d6
Dipankar Sau <akashsau2005@gmail.com>
2026-08-15 12:08:50 +0530

    add new folder

--- Membership.operators.py/Member_operator.py
+++ Membership.operators.py/Member_operator.py
@@ -1,1 +1,16 @@
 nums=[4,7,3,8,1,1,2,10,9,6,1,1,1]
+# print(100 in nums)
+# target=int(input("enter a number"))
+# if target in nums:
+#    nums.remove(target)
+#    print(nums)
+# else:
+#    print("target does not exit")
+
+def greet(target):
+   if target in nums:
+      nums.remove(target)
+      return nums
+nums=[4,7,3,8,1,1,2,10,9,6,1,1,1]
+target=int(input("enter a number"))
+print(greet(target))
