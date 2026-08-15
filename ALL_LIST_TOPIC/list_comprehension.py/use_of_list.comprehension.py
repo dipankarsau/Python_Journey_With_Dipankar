5ea525b45f64a32a14ee76f1db70023d9a9588f3
Dipankar Sau <akashsau2005@gmail.com>
2026-08-15 16:49:20 +0530

    add new folder

--- ALL_LIST_TOPIC/list_comprehension.py/use_of_list.comprehension.py
+++ ALL_LIST_TOPIC/list_comprehension.py/use_of_list.comprehension.py
@@ -1,7 +1,24 @@
-# Using list comprehension, create a list of squares of all odd numbers from 1 to 20.
 
-# Expected output:
-# [1, 9, 25, 49, 81, 121, 169, 225, 289, 361]
 
 square=[i*i for i in range(1,11)]
 print(square)
+
+
+
+# 1to 20 , but only even number
+
+new=[i for i in range(1,21) if i%2==0]
+print(new)
+
+# from 1 to 100 mak a list of all prime number
+
+def is_prime(num):
+    count=0
+    for i in range(1,num+1):
+        if i%2==0:
+            count+=1
+    if count==2:
+        return True
+    return False
+new_lst=[i for i in range(1,101) if is_prime(i)]
+print(new_lst)
