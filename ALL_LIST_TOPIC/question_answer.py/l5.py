e1d6b79e3d5f7035b725dccf125286dcda8e3492
Dipankar Sau <akashsau2005@gmail.com>
2026-08-15 13:39:19 +0530

    add new folder

---
+++ ALL_LIST_TOPIC/question_answer.py/l5.py
@@ -0,0 +1,28 @@
+
+    # Create a list containing the squares of numbers
+    # from 1 to 10 (i.e., [1, 4, 9, ... , 100]).
+
+    # Expected output:
+    # [1,4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# num=[1,2,3,4,5,6,7,8,9,10]
+# new=[]
+# for i in num:
+#     new.append(i*i)
+# print(new)
+
+
+    # using function
+# def greet(num):
+#     new = []
+
+#     for i in num:
+#         new.append(i * i)
+
+#     return new
+
+
+# num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# print(greet(num))
+        
