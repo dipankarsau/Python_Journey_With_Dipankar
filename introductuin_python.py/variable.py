76e0a0723f9e173a6e15cc0e2abb888c72c7ddaf
Dipankar Sau <akashsau2005@gmail.com>
2026-08-16 11:21:38 +0530

    add new folder

---
+++ introductuin_python.py/variable.py
@@ -0,0 +1,41 @@
+# What is a Variable?
+
+# A variable is a name that stores a value in memory. Think of it as a labelled box
+# you give the box a name, put something inside, and refer to it
+# whenever you need it.
+
+# name = "Rahul"
+# age = 21
+# gpa = 8.5
+# is_student = True
+
+# print (name)
+# print (age)
+
+# Note: Python does not require you to declare the type of a variable.
+# It figures it out automatically based on the value you assign.
+
+
+
+
+
+
+# Variable Naming Rules
+
+# V Must start with a letter or underscore (_)
+# V Can contain letters, digits, and underscores
+# x Cannot start with a digit
+# X Cannot use Python keywords (if, for, while ... )
+# Names are case-sensitive -> age # Age # AGE
+
+# # Valid
+# student_name = "Priya"
+# _count = 10
+# totalMarks2 = 95
+
+# # Invalid
+# 2marks = 95
+# my-name = "Raj"
+
+# # starts with digit
+# # hyphens not allowed
