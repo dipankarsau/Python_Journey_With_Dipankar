6ee06ce85b93163af5f474a5f0cb10493f30e774
Dipankar Sau <akashsau2005@gmail.com>
2026-08-19 21:53:57 +0530

    add new folder

---
+++ Tuple.py/Tuple_packing_and_unpacking.py
@@ -0,0 +1,42 @@
+# YTHOM RALS
+
+# Tuple Packing and Unpacking
+# Tuple pucking and unpacking are powrful featuns in Python that allow you to aanign muit ple values
+# Packing: Grouping Values into a Tuple
+# Packing occurs when multiple values ane amsigned to a zingle vartable without eaplcitly uning parenthees. Python autsmatically groupt thean valunt into a tuple
+
+# #Packing
+# person = "Rahur", 21, "Delr
+# printiperion)
+# print(typelperson()
+
+# Unpacking: Extracting Values from a Tuple
+# Unpacking in the newetse proceex, where elerrente of a tuple ar ansigned to indvidual variabkes. The numter of variabkes on the left aide of the axsignment must esactly match the number of wlernente in the tuple.
+
+# person = [Rahur", 21, "Delhr"]
+
+# . Unpacking
+# nare, age, city = perian
+# printjname) # Dutput: Rahul
+# Dutput: 21
+# # Dutput: Dehl
+
+# iable (packiing) or wat
+
+# into anp
+
+# packing). Thene aperation
+
+# code and ane fundamental to how tuples arn uned
+
+# #Dutput: ('Rahul, 21, Delhr)
+# # Dutput: dms tuple'y
+
+# pnnaagel
+
+# Important: If the mumteer of variabks dkws not match the number of elernentx in the tuphe, Python wil taise a ValueErrar.
+
+# point =(10, 20, 30)
+# #x, y= paint #This line would raiue a Valuetrror: too mary values ta unpack
+
+# Triok". Flenant Vartahle Syannion
---
+++ Tuple.py/packing_unpacking.py
@@ -0,0 +1,8 @@
+# my_tuple=45,32,11,"anirudh","khurana","surat",99
+my_tuple=1,
+# print(my_tuple,type(my_tuple))
+a,b,c,d=1,2,3,4
+print(a,type(a))
+print(a,type(b))
+print(a,type(c))
+print(a,type(d))
