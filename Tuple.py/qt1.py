542c9a01e8d1afa38f2603e16fa3e0d511a251de
Dipankar Sau <akashsau2005@gmail.com>
2026-08-19 22:05:22 +0530

    add new folder

---
+++ Tuple.py/qt1.py
@@ -0,0 +1,4 @@
+#Q74: Create a tuple of 5 of your favourite songs. Print the first, last, and middle one using indexing.
+my_tuple=("akash","sujat","guru","tuple","song5")
+n=len(my_tuple)
+print(my_tuple[0],my_tuple[-1],my_tuple[n//2])
