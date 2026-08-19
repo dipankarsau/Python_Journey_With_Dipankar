063d68c22f22f1e657445ede0a04c9948cfb8dd2
Dipankar Sau <akashsau2005@gmail.com>
2026-08-19 23:15:07 +0530

    add new folder

--- Dictionary.py/accesses element.py
+++ Dictionary.py/accesses element.py
@@ -1,1 +1,11 @@
-ma
+marks={"science":99,"maths":100,"hindi":43,"history":71,}
+# print(marks["science"])
+# a=marks.get("science")
+# print(a)
+subject="history"
+ans= marks.get(subject)
+if ans is None:
+
+    print("subject not found")
+else:
+    print(f"marks scored ={ans}")
