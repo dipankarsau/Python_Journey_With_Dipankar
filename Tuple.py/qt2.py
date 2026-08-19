2be8e934e1e024bb6683c6d8a22959a435b02f4f
Dipankar Sau <akashsau2005@gmail.com>
2026-08-19 22:11:07 +0530

    add new folder

---
+++ Tuple.py/qt2.py
@@ -0,0 +1,3 @@
+#Q75: Create a tuple of 8 numbers. Using slicing, print the first 3, last 3, and every alternate element.
+my_tuple=(1,2,3,4,5,6,7,8)
+print(my_tuple[:3],my_tuple[-3:],my_tuple[::2])
