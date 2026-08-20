05f859aa64264ee5b6366cc5c6521145578057be
Dipankar Sau <akashsau2005@gmail.com>
2026-08-21 02:22:01 +0530

    add new folder

---
+++ Dictionary.py/Nested_Dictionaries 2.py
@@ -0,0 +1,43 @@
+# NTHOH MADCT
+
+# Nested Dictionaries (Part 2: List Inside Dict)
+# lm addidi ticn tio dictionaries, lita can alno nerwe ait vallaes within anather dictionary: Thie powertul combinanion allown a ningle kay to itore muitiple wianed
+# pleceu off infformation, perfict for mpesenting collectionn of terna an sociated with a primary entity lke a utudenta ervaled mubjecta ora department'u staff
+
+# #Example i: Student profile with lats for subjects and marks.
+# uudent =
+# "nama": "Rahul,
+# "aga": 21,
+# "wbjecti":["Math", "Science", "Inglih"]
+
+# #Accesting lut elementt intide a dctionary
+# princitudant"wbject"] #Output: ['Math, Science, Tirgah]
+# princitudant"wbjectr"]o0 # Output Mach
+
+# #Modfying a liut dinecdy within the dictionary
+
+# mudent['markr"]append70]
+# princjitudent)
+# # Dupu fname: Rahuf, "age': 21, subject [Math, Science', Engish', Hind], mrark': [26, 92, 78, 700
+
+# #Example 2: Raal-world senaro . company departments with employee luts.
+
+# "engineering:['Rahuf, "Priya", "Karan"],
+# "mareting": [Sarw","Amman"]
+# ['Neha"]
+
+# 4
+
+# #Looping through depamment and employes luts
+# for dept, emplayeec in compary.te mu:
+# prinat ydept.uppend-fenjempbyweig membi]
+# for emp in empiayeec
+
+# # Duput
+# #ENGINEERING-2 membiE
+
+# # -Karan
+# MARKETING-2 membert:
+
+# # -Aman
+# #HR-1 mimbert:
---
+++ Dictionary.py/list in dict.py
@@ -0,0 +1,10 @@
+student = {
+"name": "Rahul",
+"age": 21,
+"subjects":["Math", "Science", "English"],
+"marks": [85, 92, 78],
+}
+# print(student["name"])
+# print(student["subjects"][0])
+# print(student["subjects"][-1])
+print(sum(student["marks"]))
