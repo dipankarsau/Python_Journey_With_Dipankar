0c94355ada69d8d21b05eeec99695a0888d09e5d
Dipankar Sau <akashsau2005@gmail.com>
2026-08-23 10:28:50 +0530

    add new folder

---
+++ SETS/basic.py
@@ -0,0 +1,5 @@
+my_set={5,5,2,4,5,5,6,5,"anorudh","khurana",99.99,5}
+print(my_set,type(my_set))
+# how to create empty set
+set=set()
+print(set,type(set))
---
+++ SETS/type conversion.py
@@ -0,0 +1,7 @@
+my_set={5,5,2,4,5,5,6,5,"anorudh","khurana",99.99,5}
+print(list(my_set))
+
+
+my_list=[54,65,43,32,32,54,65]
+a=set(my_list)
+print(list(a))
