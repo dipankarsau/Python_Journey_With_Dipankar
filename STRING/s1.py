d31c040fc51ee5f657ec1fbff6a540474b10a4a4
Dipankar Sau <akashsau2005@gmail.com>
2026-08-23 14:39:33 +0530

    add new folder

---
+++ STRING/s1.py
@@ -0,0 +1,5 @@
+# Q93. Take a name as input from the user. Print its first character, its last
+# character, and the total length of the name.
+name="programing"
+n=len(name)
+print(name[0],name[-1],n)
