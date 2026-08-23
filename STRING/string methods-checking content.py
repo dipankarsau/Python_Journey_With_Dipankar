50da97f49edecdff9249ccfd89f0cd01376f6119
Dipankar Sau <akashsau2005@gmail.com>
2026-08-23 15:25:26 +0530

    add new folder

---
+++ STRING/string methods-checking content.py
@@ -0,0 +1,24 @@
+# CHECKING CONTENT (T/F)
+# isalpha(): All Letters?
+# isdigit(): All Digits?
+# isalnum(): AlphaNumeric Only?
+# isspace(): All Whitespace?
+# startswith() and endswith()
+
+# text="anirudh khurana"
+
+# print(text.isalpha())
+# print(text.isdigit())
+# print(text.isspace())
+# print(text.startswith("a"))
+# print(text.endswith("a"))
+# abc="abc"
+# print(abc.isalnum())
+# age=input("enter your age:-")
+# if age.isdigit():
+#     if int(age)>=18:
+#         print("you are eligible")
+#     else:
+#         print("yiu are not eligible")
+# else:
+#     print("enter your poprer format way age like 18,19")
