e8ef041b68f82d46541967bab9e0eeab6b03126a
Dipankar Sau <akashsau2005@gmail.com>
2026-08-23 13:48:13 +0530

    add new folder

---
+++ STRING/introductio string.py
@@ -0,0 +1,24 @@
+# PYTHON BASICS
+
+# Strings: Quick Recap and What's Next
+
+# We briefly encountered strings in an earlier session. Let's quickly recap their creation and basic usage before delving deeper into their powerful features.
+
+# # Creating strings - single, double, or triple quotes
+# name = "Rahul"
+# city = 'Delhi"
+# message = *This is a
+# multi-line string"*"
+
+# # Printing strings
+# print(name)
+# print(f"Hello, {name} from {city}") # f-string
+
+# # Strings can contain any character
+# address = "Flat No. 404, Pune - 411001*
+
+# In this session, we'll explore strings much more comprehensively - how to access individual characters, slice specific portions, search for patterns, modify
+# case, split them into pieces, and utilise the numerous built-in methods Python offers for text manipulation.
+
+# Strings are fundamental in real-world programming, appearing in usernames, emails, file content, API responses, and user input. Mastering them is essential
+# for any Python developer.
