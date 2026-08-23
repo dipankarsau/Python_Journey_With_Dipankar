7aca9c9f5b353a417182b8f989dd6ab848b89298
Dipankar Sau <akashsau2005@gmail.com>
2026-08-24 01:16:21 +0530

    add new folder

---
+++ STRING/S9.py
@@ -0,0 +1,9 @@
+# Q101: File Type Check
+# Take a filename as input (like report.pdf). Check if it ends with .pdf,
+# .docx, or .txt and print the file type.
+
+filename = "report.pdf"
+if filename.endswith(".pdf") or filename.endswith(".docx") or filename.endswith(".txt"):
+    print("valid file")
+else:
+    print('please select valid file type')
