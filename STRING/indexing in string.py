59fd9be79dd3a3f653ce699bbe0856713d83459f
Dipankar Sau <akashsau2005@gmail.com>
2026-08-23 14:05:00 +0530

    add new folder

---
+++ STRING/indexing in string.py
@@ -0,0 +1,33 @@
+# INTIOH ANDCI
+
+# Indexing Strings
+# Grety character in a mtring har a apecific ponition, known an an
+# types off indraing
+
+# Positive Indexing (Left to Right)
+# Indeoes start thom d for the first character and increane by ane for each mubaequent chracter towarde the tight.
+
+# name= "Python"
+# # 012345 (Pouths indeues)
+
+# prina namejo . Dutput: P
+# prira name[] . Dutput:h
+# prira namej'] . Dutput:n
+
+# x Thia albwt youta acceee individual characten or parts of a itring. Python uonstwo
+
+# Negative Indexing (Right to Left)
+# Indeoes start from -1 for the llant cracter and decreate ax you mave towardi the lft.
+
+# name= "Python"
+# # 4-5-4-3-2-1(Negatve Indexs
+
+# prira namnef-1]) # Outpuc i
+# prira name[-7[) # Outpuc I
+# arira namef-C0) # Oupuc P
+
+# Thie allows for corvenient accetn tto daractere from either end off the utring. For sxarngle, to pet the firit and latt chacter of a word
+
+# word = inputfErter a wand:"1
+# priraf Finutt character: |wond(o(")
+# prinaf Laut character: (word:11F")
---
+++ STRING/string slicing.py
@@ -0,0 +1,10 @@
+text="programing"
+print(text[::-1])  # reverse a string
+print(text[3::])
+print(text[0:4])
+print(text[4:10])
+print(text[:])
+print(text[10:5:-1])
+print(text[7::-1])
+print(text[7::1])
+print(text[::2])
