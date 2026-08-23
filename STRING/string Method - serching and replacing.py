4b675ff1069f0837b8ba7df4417ff5eeed5f0bc2
Dipankar Sau <akashsau2005@gmail.com>
2026-08-23 15:39:24 +0530

    add new folder

---
+++ STRING/string Method - serching and replacing.py
@@ -0,0 +1,15 @@
+# String Methods: Searching and Replacing
+# count(): Substring Frequency
+# find(): Locate First Occurrence
+# index(): Locate (with Error)
+# replace(): Substitute Substrings
+
+
+text="anirudh khurana"
+print(text.count("a"))
+print(text.count("an"))
+print(text.find("a"))
+print(text.find("khurana"))
+print(text.index("a"))
+print(text.replace("a","z"))
+print(text.replace("a","n"))
