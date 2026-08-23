c8f6418883beb3010f8998bb2b54fb77b48228ef
Dipankar Sau <akashsau2005@gmail.com>
2026-08-23 15:00:48 +0530

    add new folder

--- STRING/s1.py
+++ STRING/s1.py
@@ -1,5 +1,12 @@
 # Q93. Take a name as input from the user. Print its first character, its last
 # character, and the total length of the name.
-name="programing"
-n=len(name)
-print(name[0],name[-1],n)
+# name="programing"
+# n=len(name)
+# print(name[0],name[-1],n)
+
+
+# using function
+def greet(name):
+    n=len(name)
+    return name[0], name[-1],n
+print(greet("programing"))
