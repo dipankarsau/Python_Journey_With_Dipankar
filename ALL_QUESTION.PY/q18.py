3d0641fc57e26e26f3cbae374b644945bb76d136
Dipankar Sau <akashsau2005@gmail.com>
2026-08-26 02:06:50 +0530

    add new folder

---
+++ ALL_QUESTION.PY/q18.py
@@ -0,0 +1,13 @@
+# Ask a number from the user, print the multiplication table upto 10
+# using for loop
+# num=int(input("enter your number:-"))
+# for i in range(1,11):
+#     print(f" {num} * {i}= {num*i}")
+
+
+#  using while loop
+num=int(input("enter your number:-"))
+i=1
+while i<=10:
+     print(f" {num} * {i}={num*i}")
+     i=i+1
