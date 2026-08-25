54d085bff07fb5adbb6ee7699d62a0f27b7dd62d
Dipankar Sau <akashsau2005@gmail.com>
2026-08-25 23:17:02 +0530

    add new folder

---
+++ ALL_QUESTION.PY/q6.py
@@ -0,0 +1,9 @@
+# Take a number as input. Print the result of that number raised to the
+# power of 3 using **. Also print what // 7 and % 7 give for the same number
+
+
+user=int(input("enter a number:-")) 
+print(user**3,user//7,user%7)
+
+
+
