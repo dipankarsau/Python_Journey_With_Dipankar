a0ddbe963a8005d37def6e9953a53a93729bb59c
Dipankar Sau <akashsau2005@gmail.com>
2026-08-26 01:53:48 +0530

    add new folder

---
+++ ALL_QUESTION.PY/q17.py
@@ -0,0 +1,15 @@
+# Sum of all the numbers from 1 to 100
+# using for loop
+
+# sum=0
+# for i in range(1,101):
+#     sum=sum+i
+# print(sum)
+
+# using while loop
+# sum=0
+# i=1
+# while i<=100:
+#     sum=sum+i
+#     i=i+1
+# print(sum)
