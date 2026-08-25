0bcec79ebfbe45fdba0be87219c9b6645d92b9f1
Dipankar Sau <akashsau2005@gmail.com>
2026-08-25 23:00:10 +0530

    add new folder

---
+++ ALL_QUESTION.PY/q4.py
@@ -0,0 +1,15 @@
+# Take the user
+# '
+# s age as input. Check and print whether they are eligible
+# to vote (age >= 18) and whether they are a senior citizen (age >= 60).
+# Print both results.
+
+
+
+
+user=int(input('enter your age:-'))
+
+
+a=user>=18
+b=user>=60 
+print(f" you are eligible for vote you age is but {a}   you are senior citizen {b} ") 
