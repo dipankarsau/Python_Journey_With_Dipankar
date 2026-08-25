7dc3ef5a6e276638c373af6a6b34bd00d9f760f3
Dipankar Sau <akashsau2005@gmail.com>
2026-08-25 23:34:07 +0530

    add new folder

---
+++ ALL_QUESTION.PY/q8.py
@@ -0,0 +1,10 @@
+#  Take a number as input. Print whether it is positive, negative, or zero
+num=int(input("input a number"))
+if num>0:
+    print("it is positive number")
+elif num<0:
+    print("it is negative number")
+else:
+    print("it is a zero")
+
+
