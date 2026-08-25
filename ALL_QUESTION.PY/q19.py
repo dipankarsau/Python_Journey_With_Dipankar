b4b9791b5c33ac6c2363ec8e0dc6b05d1df10c28
Dipankar Sau <akashsau2005@gmail.com>
2026-08-26 02:20:32 +0530

    add new folder

---
+++ ALL_QUESTION.PY/q19.py
@@ -0,0 +1,16 @@
+# Ask a number from the user, and print all the factors.
+# using for loop
+# num1=int(input("enter your number:-"))
+# for i in range(1,num1):
+#     if num1%i==0:
+#         print(f"the factor of num1 is {i}")
+
+
+# using  while loop
+# num1=int(input("enter your number:-"))
+# i=1
+# while i<=num1:
+#     if num1%i==0:
+#           print(f"the factor of num1 is {i}")
+#     i=i+1
+
