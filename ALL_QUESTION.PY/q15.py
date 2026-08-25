5de177287e8b8615bfc89ef01ae5915f5ed1e53e
Dipankar Sau <akashsau2005@gmail.com>
2026-08-26 01:51:30 +0530

    add new folder

---
+++ ALL_QUESTION.PY/q15.py
@@ -0,0 +1,15 @@
+# Print all the numbers which are divisible by 3 and 5, from 1 to 100.
+
+# for loop
+# for i in  range(1,101):
+#     if i%3==0 and i%5==0:
+#         print(i)
+# while loop
+i = 1
+
+# while i <= 100:
+
+#     if i % 3 == 0 and i % 5 == 0:
+#         print(i)
+
+#     i = i + 1
