798975c2d96b825fbedffa3ef019868e61f81f27
Dipankar Sau <akashsau2005@gmail.com>
2026-08-26 01:45:43 +0530

    add new folder

---
+++ ALL_QUESTION.PY/q14.py
@@ -0,0 +1,11 @@
+# Take a number as input. Using the ternary operator, print "Even
+
+
+user=int(input("enter your number:-"))
+
+if user%2==0:
+    print("even number")
+else:
+    print("odd number")
+
+
