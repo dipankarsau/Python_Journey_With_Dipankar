daa94cc69bd4a2108b6e9d118a160978cba9b524
Dipankar Sau <akashsau2005@gmail.com>
2026-08-26 20:17:49 +0530

    add new folder

---
+++ ALL_QUESTION.PY/q21.py
@@ -0,0 +1,14 @@
+# Q21. Print the following pattern using nested loops:
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print('*',end=" ")
+    print()
+    
