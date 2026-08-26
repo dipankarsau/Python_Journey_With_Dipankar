139c09b91d39c3c6e5f719f47dfbac1521246572
Dipankar Sau <akashsau2005@gmail.com>
2026-08-26 22:36:59 +0530

    add new folder

---
+++ ALL_QUESTION.PY/q31.py
@@ -0,0 +1,11 @@
+# Q32. Print the following pattern using nested loops:
+
+# 1 2 3 4 5
+# 1 2 3 4 5
+# 1 2 3 4 5
+# 1 2 3 4 5
+# 1 2 3 4 5
+for i in range(1,6):
+    for j in range(1,6):
+        print(j, end=' ')
+    print()
