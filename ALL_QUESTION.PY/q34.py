525a07f52405aba5a0323ae3bb8404f9390b1166
Dipankar Sau <akashsau2005@gmail.com>
2026-08-27 21:09:02 +0530

    add new folder

--- ALL_QUESTION.PY/q34.py
+++ ALL_QUESTION.PY/q34.py
@@ -1,9 +1,11 @@
 # Q36. Write a function that print all the factors of a number entered by user.
 
-def greet(num):
-    factor=1
-    for i in range(1,num+1):
-        factor=factor*i
-    return factor
-num=int(input("enter a number"))
-print(greet(num))
+def greet():
+    num = int(input("enter a number: "))
+
+    for i in range(1, num + 1):
+        if num % i == 0:
+            print(i, end=" ")
+
+
+greet()
---
+++ ALL_QUESTION.PY/q35.py
@@ -0,0 +1,10 @@
+# Q37. Write a function called add that takes two numbers as parameters
+# and prints their sum.
+
+def add(a,b):
+    
+    print(a+b,end='')
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+add(a,b)
+
