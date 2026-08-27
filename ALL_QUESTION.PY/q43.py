96b5c904ff0bd4506092a89236b0ad4c87c8c72d
Dipankar Sau <akashsau2005@gmail.com>
2026-08-28 01:30:47 +0530

    add new folder

---
+++ ALL_QUESTION.PY/q43.py
@@ -0,0 +1,4 @@
+# Create a list of 5 of your favourite movies. Print the first, last, and middle movie from your list using both positive and negative indexing where appropriate.
+movies = ["Movie1", "Movie2", "Movie3", "Movie4", "Movie5"]
+n=len(movies)
+print(movies[0],movies[n//2], movies[-1])
