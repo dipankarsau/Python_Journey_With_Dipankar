11bcbc5b04b1343bc405957083567fe7a32c4a11
Dipankar Sau <akashsau2005@gmail.com>
2026-08-27 21:11:27 +0530

    add new folder

---
+++ ALL_QUESTION.PY/q36.py
@@ -0,0 +1,8 @@
+# Q38. Write a function called rectangle_area that takes length and breadth
+# as parameters and prints the area.
+
+def rectangle_area(length,breadth):
+    print(length*breadth)
+
+
+rectangle_area(2,3)
