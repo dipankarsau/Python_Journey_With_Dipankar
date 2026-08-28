00ac441f8805bffe6c3d80ffa61fdf03883dd510
Dipankar Sau <akashsau2005@gmail.com>
2026-08-28 20:49:53 +0530

    add new folder

---
+++ ALL_QUESTION.PY/q51.py
@@ -0,0 +1,16 @@
+# Q59. Reverse a list without using the
+# .reverse() method or list slicing
+# ([ :: -1]).
+
+# Hint: Think about swapping elements from
+# both ends of the list using a loop.
+# Example: [1, 2, 3, 4, 5] -> [5, 4, 3, 2, 1]
+
+
+a=[1, 2, 3, 4, 5]
+n=len(a)
+new=[]
+for i in range(n,0,-1):
+    new.append(i)
+print(new)
+    
