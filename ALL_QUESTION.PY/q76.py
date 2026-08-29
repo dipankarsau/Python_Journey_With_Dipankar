6c48058ab73cc0a70ed55f318e21f1671f3e38e9
Dipankar Sau <akashsau2005@gmail.com>
2026-08-29 13:57:38 +0530

    add new file

---
+++ ALL_QUESTION.PY/q76.py
@@ -0,0 +1,10 @@
+# Q79: Student Profile (HW)
+# Create a dictionary for a student, including keys like name, age, city, and
+# marks (as a list of scores). Print each piece of information using its key.
+student = {
+    "name": "Dipankar",
+    "age": 21,
+    "city": "Kolkata",
+    "marks": [85, 90, 88]
+}
+print(student["name"],student["age"],student["city"],student["marks"])
