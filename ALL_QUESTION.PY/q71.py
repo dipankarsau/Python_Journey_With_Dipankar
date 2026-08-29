7a1afb0ffd9d0dd31d9bd63b405c6e594c01a2d8
Dipankar Sau <akashsau2005@gmail.com>
2026-08-29 12:30:35 +0530

    add new file

---
+++ ALL_QUESTION.PY/q71.py
@@ -0,0 +1,5 @@
+# Create a tuple of 5 of your favourite songs. Print the first, last, and middle one using indexing.
+
+songs = ("Kesariya", "Tum Hi Ho", "Apna Bana Le", "Heeriye", "Chaleya")
+n=len(songs)
+print(songs[0] , songs[n//2], songs[-1])
