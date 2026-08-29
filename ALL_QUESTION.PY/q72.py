258d7c69c6860efdadc46a7485fa757221cc3b14
Dipankar Sau <akashsau2005@gmail.com>
2026-08-29 12:34:03 +0530

    add new file

---
+++ ALL_QUESTION.PY/q72.py
@@ -0,0 +1,3 @@
+# Create a tuple of 8 numbers. Using slicing, print the first 3, last 3, and every alternate element.
+numbers = (10, 20, 30, 40, 50, 60, 70, 80)
+print(numbers[:3], numbers[-3:], numbers[::2])
