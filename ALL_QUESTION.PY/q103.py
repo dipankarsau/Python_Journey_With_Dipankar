3c0abb20b3c1c2c397ee427f81f87237a3020499
Dipankar Sau <akashsau2005@gmail.com>
2026-08-31 01:38:26 +0530

    add new file

---
+++ ALL_QUESTION.PY/q103.py
@@ -0,0 +1,11 @@
+# Q100. Sorted Names
+
+# Take a list of names as input (comma separated). Split them, sort them
+# alphabetically, and join them back with " | " as separator.
+
+
+
+
+names_input ="Charlie, Alice, Ethan, Bob,Diana"
+word=names_input.split(",")
+print("|".join(word))
