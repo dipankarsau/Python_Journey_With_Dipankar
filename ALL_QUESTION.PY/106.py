ab1bc25bb8ec2801ca39201a3f7f6d9aa5454ca1
Dipankar Sau <akashsau2005@gmail.com>
2026-09-01 01:35:08 +0530

    add new file

---
+++ ALL_QUESTION.PY/106.py
@@ -0,0 +1,8 @@
+# Q104. Unique Words Count
+
+# Take a paragraph as input. Print the count of unique words in it
+# (case insensitive).
+
+paragraph = "Python is great and Python is fun and learning Python is the best"
+paragraph=paragraph.split()
+print(len(set(paragraph)))
