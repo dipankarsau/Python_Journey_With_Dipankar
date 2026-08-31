45a309cabd5ed7a17c4e71021ce52eea3046705d
Dipankar Sau <akashsau2005@gmail.com>
2026-09-01 02:12:15 +0530

    add new file

---
+++ ALL_QUESTION.PY/108.py
@@ -0,0 +1,10 @@
+# Q107. Reverse Word Order
+
+# Take a sentence as input. Reverse the order of words (not the characters in
+# each word). Example: "Python is fun" -> "fun is Python".
+
+
+example="fun is python"
+example=example.split()
+example=example[::-1]
+print(" ".join(example))
