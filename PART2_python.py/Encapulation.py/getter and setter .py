eb6e8abfd4b050cb097aa78a9a02959bc1c57965
Dipankar Sau <akashsau2005@gmail.com>
2026-09-08 01:40:06 +0530

    add new file

--- PART2_python.py/Encapulation.py/getter and setter .py
+++ PART2_python.py/Encapulation.py/getter and setter .py
@@ -1,8 +1,20 @@
 class Student:
-    def __init__(self,name:str):
-        self.__namee=name
-    # getter
+    def __init__(self, name: str):
+        self.__name = name
 
-s1=Student("akash")
-print(s1.__namee)
-        
+    # getter
+    def get_name(self):
+        return self.__name
+
+    # setter
+    def set_name(self, new_name: str):
+        self.__name = new_name
+
+
+s1 = Student("akash")
+
+print(s1.get_name())
+
+s1.set_name("dipp")
+
+print(s1.get_name())
