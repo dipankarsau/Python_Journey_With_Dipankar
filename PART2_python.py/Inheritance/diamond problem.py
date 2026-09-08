dacd928ab5e843dd13c165ab70adaaed0d230db0
Dipankar Sau <akashsau2005@gmail.com>
2026-09-09 02:26:29 +0530

    add new file

---
+++ PART2_python.py/Inheritance/diamond problem.py
@@ -0,0 +1,23 @@
+class A:
+    def hello(self):
+        print("Hello from A")
+
+
+class B(A):
+    def hello(self):
+        print("Hello from B")
+
+
+class C(A):
+    def hello(self):
+        print("Hello from C")
+
+
+class D(B, C):
+    pass
+
+
+d = D()
+d.hello()
+
+print(D.mro())
