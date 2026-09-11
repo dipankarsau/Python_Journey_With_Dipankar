9ebfb2c9639e018cc1518da23b76b89228ccf283
Dipankar Sau <akashsau2005@gmail.com>
2026-09-12 02:02:25 +0530

    add new file

---
+++ PART2_python.py/Abstarction/how to create a abstraction.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Circle(Shape):
+
+    def __init__(self, radius: int):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+    def perimeter(self):
+        return 2 * 3.14 * self.radius
+
+
+circle = Circle(5)
+
+print("Area:", circle.area())
+print("Perimeter:", circle.perimeter())
