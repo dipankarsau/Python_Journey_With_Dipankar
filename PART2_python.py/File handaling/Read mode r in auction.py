b865773f761685c0266879ea6c83da7ed70bd67f
Dipankar Sau <akashsau2005@gmail.com>
2026-09-13 22:26:34 +0530

    add new file

--- PART2_python.py/File handaling/Read mode r in auction.py
+++ PART2_python.py/File handaling/Read mode r in auction.py
@@ -1,4 +1,17 @@
-f=open("hello.txt","r")
-content=f.read()
-print(content)
-f.close()
+# f=open("hello.txt","r")
+# content=f.read()
+# print(content)
+# f.close()
+
+
+# f=open("hello.txt","r")
+# content=f.read(10)
+# print(content)
+# content1=f.read(10)
+# print(content1)
+# f.close()
+
+
+with open("hello.txt","r") as f:
+    content=f.read(10)
+    print(content)
