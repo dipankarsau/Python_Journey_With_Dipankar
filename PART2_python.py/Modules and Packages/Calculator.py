a77197719b050049bdfc7c88d3dbb7df022fb095
Dipankar Sau <akashsau2005@gmail.com>
2026-09-15 21:32:27 +0530

    add new file

---
+++ PART2_python.py/Modules and Packages/Calculator.py
@@ -0,0 +1,11 @@
+def addition(a:int,b:int)->int:
+    return a+b
+def subtraction(a:int,b:int)->int:
+    return a-b
+def multiply(a:int,b:int)->int:
+    return a*b
+def division(a:int,b:int)->float:
+    return a/b
+
+
+PI=3.14
---
+++ PART2_python.py/Modules and Packages/main.py
@@ -0,0 +1,6 @@
+import Calculator
+print(Calculator.addition(6,7))
+print(Calculator.subtraction(6,7))
+print(Calculator.multiply(6,7))
+print(Calculator.division(9,7))
+print(Calculator.PI)
