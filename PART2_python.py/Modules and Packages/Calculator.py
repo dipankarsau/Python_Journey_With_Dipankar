7488a9a4d78278f671b80849bcbb001e7a3c81ff
Dipankar Sau <akashsau2005@gmail.com>
2026-09-15 21:54:02 +0530

    add new file

--- PART2_python.py/Modules and Packages/Calculator.py
+++ PART2_python.py/Modules and Packages/Calculator.py
@@ -1,11 +1,21 @@
-def addition(a:int,b:int)->int:
-    return a+b
-def subtraction(a:int,b:int)->int:
-    return a-b
-def multiply(a:int,b:int)->int:
-    return a*b
-def division(a:int,b:int)->float:
-    return a/b
+def addition(a: int, b: int) -> int:
+    return a + b
+
+def subtraction(a: int, b: int) -> int:
+    return a - b
+
+def multiply(a: int, b: int) -> int:
+    return a * b
+
+def division(a: int, b: int) -> float:
+    return a / b
 
 
-PI=3.14
+PI = 3.14
+
+if __name__ == "__main__":
+    print(f"calculator file __name__ = {__name__}")
+    result = addition(2, 3)
+    print(result)
+
+print("hello word")
--- PART2_python.py/Modules and Packages/main.py
+++ PART2_python.py/Modules and Packages/main.py
@@ -1,6 +1,8 @@
 import Calculator
-print(Calculator.addition(6,7))
-print(Calculator.subtraction(6,7))
-print(Calculator.multiply(6,7))
-print(Calculator.division(9,7))
+
+print(Calculator.addition(6, 7))
+print(Calculator.subtraction(6, 7))
+print(Calculator.multiply(6, 7))
+print(Calculator.division(9, 7))
 print(Calculator.PI)
+
