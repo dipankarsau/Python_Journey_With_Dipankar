bb02e1d8ee1355eb59e3628005821bb2f7102cdf
Dipankar Sau <akashsau2005@gmail.com>
2026-09-19 21:12:28 +0530

    add new file

---
+++ PART2_python.py/Multithreading/basic.py
@@ -0,0 +1,44 @@
+# import time
+# import threading
+
+# def task():
+#     print("This task is running")
+
+#     time.sleep(2)
+
+#     print("Task finished\n")
+
+# print(" main program start\n")
+# t = threading.Thread(target=task)
+# t.start()
+
+
+
+# print("The program is finished\n")
+
+
+
+
+
+
+import time
+import threading
+
+def task(name):
+    print(f'{name}"This task is running"')
+
+    time.sleep(2)
+
+    print(f" {name}Task finished\n")
+
+print(" main program start\n")
+t1 = threading.Thread(target=task,args=("cooking",))
+t2 = threading.Thread(target=task,args=("braking",))
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+
+
+
+print("The program is finished\n")
