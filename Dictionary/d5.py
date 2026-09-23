5b4d3a985cd474ad3e579bd9a5f1340c6f6e48f6
Dipankar Sau <akashsau2005@gmail.com>
2026-09-23 23:00:45 +0530

    add new file

--- Dictionary/d5.py
+++ Dictionary/d5.py
@@ -2,7 +2,6 @@
 # Given a dictionary of marks for different subjects, loop over its values()
 # to calculate and print the total marks and the average mark obtained.
 
-
 marks = {
     "maths": 85,
     "science": 90,
@@ -10,9 +9,14 @@ marks = {
     "computer": 95,
     "history": 72
 }
-n=len(marks)
-total=0
-for k,v in marks.items():
-    total=total+v
-    averager=total/n
-print(total,averager)
+
+n = len(marks)
+total = 0
+
+for k, v in marks.items():
+    total = total + v
+
+average = total / n
+
+print("Total Marks:", total)
+print("Average Marks:", average)
